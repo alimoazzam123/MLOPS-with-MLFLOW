bc87a8ffc3cce0b7fd4545eabce25e8f7e65c0ca
Moazzam <mdmoazzamali984@gmail.com>
2025-03-23 04:08:23 +0530

    Mlflow autolog

---
+++ mlartifacts/906678527931930630/4ee6bd0cb3c54e75aa9583d06e44d23c/artifacts/file1.py
@@ -0,0 +1,62 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set tracking URI
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Load Wine dataset
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+
+# Define the params for RF model
+max_depth = 10
+n_estimators = 8
+
+#Mention your experiment name
+mlflow.set_experiment("MLOPS-Exp1")
+
+with mlflow.start_run():
+    # Create a RandomForest model
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_test)
+
+    # Log model metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimators", n_estimators)
+
+    # Creating a confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(10,7))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=wine.target_names, yticklabels=wine.target_names)
+    plt.xlabel('Predicted')
+    plt.ylabel('Truth')
+    plt.title('Confusion Matrix')
+    
+    # Save the confusion matrix plot
+    plt.savefig('confusion_matrix.png')
+    plt.close()
+
+    # Log the confusion matrix as an artifact
+    mlflow.log_artifact('confusion_matrix.png')
+    mlflow.log_artifact(__file__)  # Logs your Python script
+    
+    #tags
+    mlflow.set_tags({"Author":"Moazzam", "Project" : "Wine Classification"})
+
+    #log the model
+    mlflow.sklearn.log_model(rf, "random-forest-model")
+
+    print(f"Accuracy: {accuracy}")
